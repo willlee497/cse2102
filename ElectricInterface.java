78f8440a96234a2c13661bc6246e70045420a8b2
willlee497 <162823988+willlee497@users.noreply.github.com>
2024-09-11 23:09:57 -0400

    Add files via upload

---
+++ ElectricInterface.java
@@ -0,0 +1,10 @@
+
+public interface ElectricInterface {
+
+    double calcMPGe();
+    void setElectricMiles(double totalElectricMiles);
+    void setTotalkWh(double totalkWh);
+
+}
+
+
---
+++ GasolineInterface.java
@@ -0,0 +1,11 @@
+public interface GasolineInterface {
+    
+    double calcGasMPG();
+    void setMilesfromGas(double miles);
+    void setGallonsfromGas(double gallons);
+    
+}
+
+
+
+
